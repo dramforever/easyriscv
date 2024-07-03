3642683902840bf8aed7eaae660764d53f20b353
dramforever <dramforever@live.com>
2024-07-03 17:51:00 +0800

    Register dump format

--- easyriscv.js
+++ easyriscv.js
@@ -90,7 +90,7 @@ function convertEmulator(el) {
         const lines = [];
         const names = "zero ra sp gp tp t0 t1 t2 s0 s1 a0 a1 a2 a3 a4 a5 a6 a7 s2 s3 s4 s5 s6 s7 s8 s9 s10 s11 t3 t4 t5 t6".split(' ');
 
-        lines.push(`pc       = ${fmt(riscv.pc)}\n`)
+        lines.push(`  pc       ${fmt(riscv.pc)}\n`)
         for (let i = 0; i < 32; i ++) {
             const end = i % 2 ? '\n' : '  |  ';
             lines.push(`${names[i].padStart(4, ' ')} ${`(x${i})`.padStart(5, ' ')} ${fmt(riscv.regs[i])} = ${riscv.regs[i].toString().padStart(10, ' ')}${end}`);
